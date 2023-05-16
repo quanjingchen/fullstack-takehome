76f5303a2df5cb7497a7b40069e3905f84e227ac
quanjingchen <quanjingchen@gmail.com>
2023-05-15 19:11:06 -0700

    modify the +page file to trigger the loadmore when user hit the bottom

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,58 +3,105 @@
 	import Loader from 'components/Loader.svelte';
 	import User from 'components/User.svelte';
 	import type { UserType } from 'lib/types';
+	import { onMount } from 'svelte';
 
 	const client = createClient({
-		url: '/graphql',
+		url: 'http://localhost:5173/graphql',
 		exchanges: [cacheExchange, fetchExchange]
 	});
 
-/*
-	const result = queryStore<{ users: UserType[] }>({
-		client,
-		query: gql`
-			query {
-				users {
-					id
-					name
-					avatar
-					email
-				}
-			}
-		`
-	});
-	*/
-
 	const MY_QUERY = gql`
-  	query MyQuery($startId: ID, $pageSize: Int!) {
-    	users(startId: $startId, pageSize: $pageSize) {
-      	id
-      	name
-      	avatar
-      	email
-    	}
-  	}
+		query MyQuery($startId: ID, $pageSize: Int!) {
+			users(startId: $startId, pageSize: $pageSize) {
+				id
+				name
+				avatar
+				email
+			}
+		}
 	`;
 
-	const result = queryStore<{ users: UserType[] }>({
-		client,
-		query: MY_QUERY,
-		variables: {
-    	startId: '1',
-    	pageSize: 10, // Replace with the desired page size
-  	},
+	let startId = 1;
+	const pageSize = 10;
+
+	let isLoading = false;
+	let users: UserType[] = [];
+
+	let scrollContainer: HTMLElement;
+
+	let loadMoreTimeout;
+
+	function loadMore() {
+		console.log('In loadMore');
+
+		// if (isLoading) return;
+
+		// isLoading = true;
+		console.log(`Is loading 1: ${isLoading}`);
+		startId = users[users.length - 1] ? Number(users[users.length - 1].id) + 1 : startId;
+		console.log(`Loading more users starting from id: ${startId}`);
+
+		client
+			.query(MY_QUERY, { startId, pageSize })
+			.toPromise()
+			.then((response) => {
+				if (response.error) {
+					console.error(response.error);
+				} else if (response.data && response.data.users) {
+					users = [...users, ...response.data.users];
+					console.log('users: ', users);
+				} else {
+					console.error('Unexpected response format:', response);
+				}
+				isLoading = false;
+				console.log(`Is loading 2: ${isLoading}`);
+			})
+			.catch((error) => {
+				console.error(error);
+				isLoading = false;
+			});
+	}
+
+	onMount(() => {
+		function handleScroll() {
+			const scrollPosition = scrollContainer.scrollTop + scrollContainer.clientHeight;
+			const scrollHeight = scrollContainer.scrollHeight;
+			const scrollThreshold = 100;
+
+			if (scrollPosition >= scrollHeight - scrollThreshold) {
+				// Clear the previous timeout if it exists
+				if (loadMoreTimeout) {
+					clearTimeout(loadMoreTimeout);
+				}
+
+				isLoading = true; // Set isLoading to true immediately
+
+				// Set a timeout to delay the loadMore call
+				loadMoreTimeout = setTimeout(() => {
+					loadMore();
+				}, 500); // 500ms delay
+			}
+		}
+
+		scrollContainer.addEventListener('scroll', handleScroll);
+
+		return () => {
+			scrollContainer.removeEventListener('scroll', handleScroll);
+		};
 	});
 
+	// Initial load
+	loadMore();
 </script>
 
-<div class="w-full h-full overflow-scroll">
+<div class="w-full h-full overflow-scroll" bind:this={scrollContainer}>
 	<div class="flex flex-col gap-4 items-center p-4">
-		{#if $result.fetching}
+		{#each users as user (user.id)}
+			<User {user} />
+		{/each}
+
+		{#if isLoading}
 			<Loader />
-		{:else if $result.data}
-			{#each $result.data.users as user (user.id)}
-				<User {user} />
-			{/each}
 		{/if}
 	</div>
 </div>
