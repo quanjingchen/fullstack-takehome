c3bd775435c78375a83bc1752594639bd364c46f
quanjingchen <quanjingchen@gmail.com>
2023-05-16 22:52:22 -0700

    feat: Enhance list loading to fill viewport

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import Loader from 'components/Loader.svelte';
 	import User from 'components/User.svelte';
 	import type { UserType, UsersPageType } from 'lib/types';
-	import { onMount } from 'svelte';
+	import { onMount, afterUpdate } from 'svelte';
 
 	const client = createClient({
 		url: 'http://localhost:5173/graphql',
@@ -38,22 +38,28 @@
 	});
 
 	$: {
-  const userStoreData = $getUserStore.data;
-  if (userStoreData) {
-		console.log(userStoreData.usersPage.users)
-    users = [...users, ...userStoreData.usersPage.users];
-		hasMore = userStoreData.usersPage.hasMore;
-  }
-}
-function nextPage() {
-	// If there are no more users to load, stop the execution of the function
-	if (!hasMore) {
-		return;
+		const userStoreData = $getUserStore.data;
+		if (userStoreData) {
+			console.log(userStoreData.usersPage.users);
+			users = [...users, ...userStoreData.usersPage.users];
+			hasMore = userStoreData.usersPage.hasMore;
+		}
 	}
+	function nextPage() {
+		// If there are no more users to load, stop the execution of the function
+		if (!hasMore) {
+			return;
+		}
 		from = from + limit;
 		console.log(`Loading more users starting from id: ${from}`);
 	}
 
+	function checkScroll() {
+		// If the fetch doesn't fill the viewport, it continues fetching until scrolling is enabled.
+		if (scrollContainer.scrollHeight <= scrollContainer.clientHeight && hasMore) {
+			nextPage();
+		}
+	}
 
 	function handleScroll() {
 		const scrollPosition = scrollContainer.scrollTop + scrollContainer.clientHeight;
@@ -61,7 +67,7 @@ function nextPage() {
 		const scrollThreshold = 100;
 		// If near the bottom of the scroll container, start loading more items
 		if (scrollPosition >= scrollHeight - scrollThreshold) {
-			// Clear the previous timeout if it exists. This can prevent multiple rapid triggers of loadMore
+			// Clear the previous timeout if it exists. This can prevent multiple rapid triggers of nextPage
 			if (nextPageTimeout) {
 				clearTimeout(nextPageTimeout);
 			}
@@ -76,16 +82,19 @@ function nextPage() {
 		}
 	}
 
+	afterUpdate(() => {
+		// Call the `checkScroll` function after each update. This ensures that the list of users always fills the entire viewport.
+		checkScroll();
+	});
 
 	onMount(() => {
-		// Initial load
-
 		scrollContainer.addEventListener('scroll', handleScroll);
 		return () => {
 			scrollContainer.removeEventListener('scroll', handleScroll);
 		};
 	});
 </script>
+
 <div class="w-full h-full overflow-scroll" bind:this={scrollContainer}>
 	<div class="flex flex-col gap-4 items-center p-4">
 		{#each users as user (user.id)}
